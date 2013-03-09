50fc9f17addafe57b69c267e2eeace501c4be5e9
Russ Taylor <russt@palantir.com>
2013-03-09 10:11:12 -0700

    Followed the basic instructions to in the help file to get a basic plugin running.

--- AmosPlugin/Class1.vb
+++ AmosPlugin/Class1.vb
@@ -1,3 +1,24 @@
-﻿Public Class Class1
+﻿Imports System.Windows.Forms
 
+Public Class Class1
+    Implements AmosGraphics.IPlugin
+    Dim WithEvents pd As AmosGraphics.pd
+
+    Public Function Description() As String Implements AmosGraphics.IPlugin.Description
+        Return "A simple example of a plugin."
+    End Function
+
+    Public Function MainSub() As Integer Implements AmosGraphics.IPlugin.MainSub
+        MsgBox("Installing a simple plugin")
+        AddHandler AmosGraphics.pd.MouseUp, AddressOf pd_MouseUp
+    End Function
+
+    Public Function Name() As String Implements AmosGraphics.IPlugin.Name
+        Return "A simple plugin"
+    End Function
+
+    Private Sub pd_MouseUp(button As MouseButtons, shift As Integer, x As Double, y As Double) Handles pd.MouseUp
+        MsgBox("You released a mouse button.")
+        RemoveHandler AmosGraphics.pd.MouseUp, AddressOf pd_MouseUp
+    End Sub
 End Class
---
+++ readme.md
@@ -0,0 +1,6 @@
+Pattern Matrix Model Builder
+============================
+
+This AMOS plugin is made with the intent to simplify the creation of models based on pattern 
+matrices in SPSS. It allows pasting the copied contents of a pattern matrix into a dialog box and 
+constructs the applicable model in AMOS.
