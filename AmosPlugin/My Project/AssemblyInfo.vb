6ad845128ab1caa9779f7605a233059eb8722b8c
Russ Taylor <russt@palantir.com>
2013-04-02 00:23:12 -0600

    Adjust titles for project settings.

--- AmosPlugin/My Project/AssemblyInfo.vb
+++ AmosPlugin/My Project/AssemblyInfo.vb
@@ -8,10 +8,10 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("AmosPlugin")> 
+<Assembly: AssemblyTitle("PatternMatrixBuilder")> 
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("AmosPlugin")> 
+<Assembly: AssemblyProduct("PatternMatrixBuilder")> 
 <Assembly: AssemblyCopyright("Copyright ©  2013")> 
 <Assembly: AssemblyTrademark("")> 
 
